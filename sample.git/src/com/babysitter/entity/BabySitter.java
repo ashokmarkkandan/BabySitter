1927fe5f1d2f2d034fe8209eda10b9f98fb9710a
ashokmarkkandan <ashokmarkkandan.gmail.com>
2015-06-30 15:58:49 -0400

    testing

---
+++ sample.git/src/com/babysitter/entity/BabySitter.java
@@ -0,0 +1,25 @@
+package com.babysitter.entity;
+
+public class BabySitter {
+    
+    public int startTime;
+    
+    public int endTime;
+    
+    public int getEndTime() {
+        return endTime;
+    }
+    
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+    
+    public int getStartTime() {
+        return startTime;
+    }
+    
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+    
+}
